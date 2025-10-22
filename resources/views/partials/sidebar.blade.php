8f927c81c991d69e65c94fc376d1ac185d4ba0a7
BimantaraDwiCahyo <27tapai@gmail.com>
2025-10-22 11:33:37 +0700

    crud driver

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -29,6 +29,32 @@
             </svg>
             <span class="menu-text">Permintaan Konsumsi</span>
         </a>
+        <a href="{{ route('admin.subbidang.index') }}"
+            class="menu-item {{ Route::is('admin.subbidang.*') ? 'active' : '' }}">
+            <svg class="menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            <span class="menu-text">Sub Bidang</span>
+        </a>
+
+        <a href="{{ route('admin.driver.index') }}" class="menu-item {{ Route::is('admin.driver.*') ? 'active' : '' }}">
+            <svg class="menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            <span class="menu-text">Driver</span>
+        </a>
+
+        <a href="{{ route('admin.jeniskendaraan.index') }}"
+            class="menu-item {{ Route::is('admin.jeniskendaraan.*') ? 'active' : '' }}">
+            <svg class="menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            <span class="menu-text">Jenis Kendaraan</span>
+        </a>
+
 
         <a href="javascript:void(0);" class="menu-item logout-button" onclick="openModal('logoutModal')">
             <svg class="menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
